742f7bd2519fcc0e71f2e1f9e403fe1284c2c803
NiuBenn <1402435102@qq.com>
2019-02-24 09:13:23 +0800

    Build inverted index

--- search/search.cc
+++ search/search.cc
@@ -14,8 +14,5 @@ int main()
 
 
     std::cout<<"Init Succes!!"<<std::endl;
-    std::cout<<s._index->Forword_Index[0]._title<<std::endl;
-    std::cout<<s._index->Forword_Index[0]._conten<<std::endl;
-    std::cout<<s._index->Forword_Index[0]._url<<std::endl;
     return 0;
 }
--- search/search.hpp
+++ search/search.hpp
@@ -4,7 +4,15 @@
 #include<fstream>
 #include<string>
 #include<vector>
+#include<map>
 #include<boost/algorithm/string.hpp>
+#include "cppjieba/Jieba.hpp"
+
+const char* const DICT_PATH = "../dict/jieba.dict.utf8";
+const char* const HMM_PATH = "../dict/hmm_model.utf8";
+const char* const USER_DICT_PATH = "../dict/user.dict.utf8";
+const char* const IDF_PATH = "../dict/idf.utf8";
+const char* const STOP_WORD_PATH = "../dict/stop_words.utf8";
 
 struct Doc_Info
 {
@@ -14,16 +22,28 @@ struct Doc_Info
     std::string _url;
 };
 
+struct Weight
+{
+    uint32_t _weight;
+    uint32_t _doc_id;
+    std::string _word;
+};
+
 class Index
 {
 public:
-    void MakeDocInfo(const std::string &input, Doc_Info& doc)
+    void MakeDocInfo(const std::string &input, Doc_Info* doc)
     {
         std::vector<std::string> output;
         boost::split(output, input, boost::is_any_of("\3"), boost::token_compress_off);
-        doc._title = output[0];
-        doc._conten = output[1];
-        doc._url = output[2];
+        doc->_title = output[0];
+        doc->_conten = output[1];
+        doc->_url = output[2];
+    }
+
+    void CutString(const std::string& s, std::vector<std::string>* words)
+    {
+        jieba.CutForSearch(s, *words);
     }
 
     bool Build(const std::string& path)
@@ -36,18 +56,64 @@ public:
             return false;
         }
 
+        uint32_t count = 0;
         while(std::getline(file,one_line))
         {
             Doc_Info doc;
             doc._id = Forword_Index.size();
-            MakeDocInfo(one_line, doc);
+            MakeDocInfo(one_line, &doc);
             Forword_Index.push_back(doc);
+        
+            Weight weight;
+            std::vector<std::string> TitleWords;
+            std::vector<std::string> ContenWords;
+
+            std::map<std::string, uint32_t> tmp;
+
+            CutString(doc._title, &TitleWords);
+            CutString(doc._conten, &ContenWords);
+
+            for(const auto& word : TitleWords)
+            {
+                if(word=="_"||word==" "||word==";"||word=="?"||word==":"||word=="."||word==","||word=="/"||word=="*"||word=="$"||word=="["||word=="]"||word=="("||word==")")
+                    continue;
+                tmp[word] += 6;
+            }
+
+            for(const auto& word : ContenWords)
+            {
+                if(word=="_"||word==" "||word==";"||word=="?"||word==":"||word=="."||word==","||word=="/"||word=="*"||word=="$"||word=="["||word=="]"||word=="("||word==")")
+                    continue;
+                tmp[word] += 1;
+            }
+
+            for(const auto& word : tmp)
+            {
+                weight._weight = word.second;
+                weight._doc_id = doc._id;
+                weight._word = word.first;
+
+                InvertedIndex[word.first].push_back(weight);
+                //std::cout<<"[weight:"<<weight._weight<<"][doc_id:"<<weight._doc_id<<"][word:"<<weight._word<<"]\n";
+            }
+
+            count++;
+            if(count%500 == 0)
+                std::cout<<"Build："<< count <<std::endl;
+
         }
+
         return true;
     }
 
+    Index():jieba(DICT_PATH,HMM_PATH,USER_DICT_PATH,IDF_PATH,STOP_WORD_PATH)
+    {}
+
 private:
     std::vector<Doc_Info> Forword_Index;
+    std::map<std::string, std::vector<Weight>> InvertedIndex;
+    cppjieba::Jieba jieba;
+    
 
 };
 
@@ -68,7 +134,6 @@ public:
 
         return true;
     }
-
 
 private:
     Index* _index;
